343436140bab8fab0a4f39a631f8e5461e50a0cd
Piotr Jander <pjander2@gmail.com>
2020-05-21 14:51:57 +0200

    Impls seat availability

--- src/main/scala/me/piotrjander/cinema/application/SeatAvailabilityProvider.scala
+++ src/main/scala/me/piotrjander/cinema/application/SeatAvailabilityProvider.scala
@@ -6,7 +6,8 @@ import cats.effect.Sync
 import cats.implicits._
 import me.piotrjander.cinema.domain.entity._
 import me.piotrjander.cinema.domain.repository._
-import me.piotrjander.cinema.main.CinemaReservations
+
+import scala.collection.mutable
 
 class SeatAvailabilityProvider[F[_]: Sync](
   reservationRepository: ReservationRepository[F],
@@ -14,6 +15,8 @@ class SeatAvailabilityProvider[F[_]: Sync](
   localClock: LocalClock[F],
   reservationRequestExpirationChecker: ReservationRequestExpirationChecker
 ) extends SeatAvailability[F] {
+
+  import SeatAvailabilityProvider._
 
   private def unconfirmedValidReservations(reservations: Seq[Reservation], requests: Seq[Option[ReservationRequest]], dateTimeNow: LocalDateTime): Seq[Reservation] =
     (reservations zip requests).foldRight(Vector.empty) {
@@ -36,6 +39,32 @@ class SeatAvailabilityProvider[F[_]: Sync](
       validReservations =
         confirmedReservations ++
           unconfirmedValidReservations(unconfirmedReservations, reservationRequests, dateTimeNow)
-    } yield ???
+    } yield calculateAvailableSeats(screening, validReservations)
+  }
+}
+
+object SeatAvailabilityProvider {
+
+  private def calculateAvailableSeats(screening: Screening, reservations: Seq[Reservation]): ScreeningSeatAvailability = {
+    val seats = reservations.flatMap(_.seats)
+    val seatMap = mutable.Map.empty[String, mutable.Map[String, Boolean]]
+
+    for (row <- screening.room.seats.keys) {
+      seatMap(row) = mutable.Map.empty[String, Boolean]
+      for (seat <- screening.room.seats(row)) {
+        seatMap(row)(seat) = false
+      }
+    }
+
+    for (Seat(row, name) <- seats) {
+      seatMap(row)(name) = true
+    }
+
+    var immutableSeatMap = Map.empty[String, Map[String, Boolean]]
+    for ((row, seats) <- seatMap) {
+      immutableSeatMap += (row -> seats.toMap)
+    }
+
+    ScreeningSeatAvailability(immutableSeatMap)
   }
 }
