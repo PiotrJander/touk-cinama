5bcc3e3b0e2faefbfcc5ffa36c5d4a0759f01d0e
Piotr Jander <pjander2@gmail.com>
2020-05-24 14:08:41 +0200

    Adds routes and controllers

---
+++ src/main/scala/me/piotrjander/cinema/infrastructure/http/ControllerUtils.scala
@@ -0,0 +1,16 @@
+package me.piotrjander.cinema.infrastructure.http
+
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives.{complete, completeOrRecoverWith}
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.directives.CompleteOrRecoverWithMagnet
+import me.piotrjander.cinema.application.exception.BadRequestException
+
+object ControllerUtils {
+
+  def completeOrBadRequest(magnet: CompleteOrRecoverWithMagnet): Route =
+    completeOrRecoverWith(magnet) {
+      case e: BadRequestException =>
+        complete(StatusCodes.BadRequest, e.getMessage)
+    }
+}
--- src/main/scala/me/piotrjander/cinema/infrastructure/http/Routes.scala
+++ src/main/scala/me/piotrjander/cinema/infrastructure/http/Routes.scala
@@ -1,35 +1,39 @@
 package me.piotrjander.cinema.infrastructure.http
 
-import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
 
 class Routes(screeningController: ScreeningController, reservationController: ReservationController) {
 
   val routes: Route =
-    pathPrefix("screenings") {
-      pathEnd {
-        get {
-          screeningController.list()
-        }
-      } ~
-      path(Segment) { screeningId =>
-        get {
-          screeningController.describe(screeningId)
-        }
-      }
-    } ~
-    pathPrefix("reservations") {
-      pathEnd {
-        post {
-          reservationController.create()
-        }
-      } ~
-      pathPrefix(Segment) { reservationId =>
-        path("confirm") {
-          post {
-            reservationController.confirm(reservationId)
+    rejectEmptyResponse {
+      pathPrefix("screenings") {
+        pathEnd {
+          get {
+            parameters(("from", "to")) { (startDateTime, endDateTime) =>
+              screeningController.list(startDateTime, endDateTime)
+            }
           }
+        } ~
+          path(Segment) { screeningId =>
+            get {
+              screeningController.describe(screeningId)
+            }
+          }
+      } ~
+        pathPrefix("reservations") {
+          pathEnd {
+            post {
+              reservationController.create()
+            }
+          } ~
+            pathPrefix(Segment) { reservationId =>
+              path("confirm") {
+                post {
+                  reservationController.confirm(reservationId)
+                }
+              }
+            }
         }
-      }
     }
 }
